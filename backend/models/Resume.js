85913b8028e35402aa6f2ee8c966a4bb258442fb
Ashish83059 <prajapatashish83059@gmail.com>
2026-02-24 13:21:56 +0530

    backend files updated

---
+++ backend/models/Resume.js
@@ -0,0 +1,84 @@
+const mongoose = require("mongoose");
+const { applyTimestamps } = require("./User");
+
+const ResumeSchema= new mongoose.Schema(
+    {
+        userId:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true,
+        },
+        title:{
+            type:String,
+            required:true,
+        },
+        thumbnailLink:{
+            types:String,
+        },
+        template:{
+            theme:String,
+            colorPalette:[String]
+        },
+        profileInfo:{
+            profilePreviewUrl:String,
+            fullname:String,
+            designation:String,
+            summary:String,
+        },
+        contactInfo:{
+            email:String,
+            phone:String,
+            location:String,
+            linkedin:String,
+            github:String,
+            website:String
+        },
+        workExperience:{
+            company:String,
+            roles:String,
+            startDate:String,
+            endDate:String,
+            description:String,
+        },
+        education:[
+            {
+                degree:String,
+                institution:String,
+                startDate:String,
+                endDate:String,
+            },
+        ],
+        skills:[
+            {
+                name:String,
+                progress:Number,
+            },
+        ],
+        projects:[
+            {
+                title:String,
+                description:String,
+                github:String,
+                liveDemp:String,
+            },
+        ],
+        certifications:[
+            {
+                title:String,
+                issuer:String,
+                year:String,
+            },
+        ],
+        languages:[
+            {
+                name:String,
+                progress:Number,
+            },
+        ],
+        interests:[String]
+
+    },
+    {timestamps:{createdAt:"createdAt", updatedAt:"updatedAt"
+    },}
+);
+module.exports=mongoose.model("Resume",ResumeSchema);
